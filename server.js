966c81ffb7903c374f7ee28c5b14d19c16e766d0
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-13 21:13:00 +0200

    Add post logic to node server example

--- server.js
+++ server.js
@@ -2,10 +2,46 @@ const http = require('node:http');
 
 const server = http.createServer(requestListener);
 
-// функція - обробник запиту від клієнту 
-function requestListener (request, response) {
-  console.log(request); // запит
-  console.log(response); // відповідь
+const users = [{ id: 1 }, { id: 2 }];
+
+// функція - обробник запиту від клієнту
+function requestListener(request, response) {
+  // console.log(request); // запит
+  // console.log(response); // відповідь
+
+  console.log(request.method); // метод запиту
+  console.log(request.url); // шлях запиту
+
+  if (request.method === 'GET') {
+    if (request.url === '/users') {
+      response.end(JSON.stringify(users));
+      return;
+    }
+
+    if (request.url === '/orders') {
+      response.end('no orders now');
+      return;
+    }
+  } else if (request.method === 'POST') {
+
+    if(request.url === '/users') {
+      let jsonString = '';
+
+      request.on('data', (chunk) => {
+        jsonString += chunk;
+      });
+
+      request.on('end', () => {
+        const newUser = JSON.parse(jsonString);
+
+        newUser.id = Date.now();
+
+        users.push(newUser);
+
+        return response.end(JSON.stringify(newUser));
+      })
+    }
+  }
 
   // відповідь на запит
   // має обов'язково бути присутня
